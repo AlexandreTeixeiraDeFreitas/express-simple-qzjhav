1e195825b4cab9fde05f9cf9758d581eb7d7223c
AlexandreTeixeiraDeFreitas <a.teixeira@ecole-ipssi.net>
2022-04-27 13:00:28 +0200

    2 files modified, 2 added

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     connectionLimit: 10,
     host: 'https://databases-auth.000webhost.com/',
-    user: '	id16564461_teixeira',
+    user: 'id16564461_teixeira',
     password: 'Bouboule2002*',
     database: 'id16564461_portfolioteixeiradefreitas',
     port: '3306',
